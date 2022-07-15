923ce8ed389ee36025842727e21cc2af8a6294c8
Lye20 <chenxf20@qq.com>
2022-07-15 14:35:24 +0800

    v3
    v3v3v3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from "@/router"
+import store from "@/store"
+import HeaderO from "@/components/HeaderO"
+import HeaderSearch from "@/components/HeaderSearch"
+import HeaderList from "@/components/HeaderList"
+import CommonFooter from "@/components/CommonFooter"
+import SimpleFooter from "@/components/SimpleFooter"
+import CommonPagination from "@/components/CommonPagination"
+import "@/assets/css/index.css"
+import "swiper/css/swiper.css"
 Vue.config.productionTip = false
+
+Vue.component("HeaderO", HeaderO)
+Vue.component("HeaderSearch", HeaderSearch)
+Vue.component("HeaderList", HeaderList)
+Vue.component("CommonFooter", CommonFooter)
+Vue.component("SimpleFooter", SimpleFooter)
+Vue.component("CommonPagination", CommonPagination)
 
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
